019c84ba02ac3bd985bfb6e5443920748a907baa
Manasa Bhat <113411403+ManasaBhat4@users.noreply.github.com>
2024-06-04 16:05:57 +0530

    Delete .github/workflows directory

--- .github/workflows/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "bucketaws137"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
--- .github/workflows/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "web" {
-  ami ="ami-04b70fa74e45c3917"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
